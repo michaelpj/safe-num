101cdf5367de6d24c3c9f605144f88ae6c0a5e5a
Michael Peyton Jones <me@michaelpj.com>
2022-04-01 22:16:05 +0100

    Work

--- src/SafeNum.hs
+++ src/SafeNum.hs
@@ -1,10 +1,13 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE MagicHash #-}
 module SafeNum where
 
+import Numeric.Natural
 import GHC.Num.Integer
 import GHC.Int
 import GHC.Real
 import Data.Ratio
+import Data.Word
 
 ----------
 -- SafeNum
@@ -23,17 +26,61 @@ class Num a => SafeNum a where
   -}
   fromIntegerMaybe :: Integer -> Maybe a
 
-
 fromIntegralMaybe :: (Integral a, SafeNum b) => a -> Maybe b
 fromIntegralMaybe = fromIntegerMaybe . toInteger
 
+-- | Implement 'fromIntegerMaybe' by checking whether the argument is inside the bounds of the type when projected into
+-- 'Integer', and returning 'Nothing' otherwise. Not universally safe, as nothing guarantees that 'fromInteger' will
+-- behave well in this range.
+fromIntegerBounded :: forall a . (Ord a, Bounded a, Integral a) => Integer -> Maybe a
+fromIntegerBounded n = if n < toInteger (minBound :: a) || n > toInteger (maxBound :: a) then Nothing else Just (fromInteger n)
+
 instance SafeNum Integer where
   fromIntegerMaybe i = Just i
+
+instance SafeNum Natural where
+  fromIntegerMaybe i | i < 0 = Nothing
+  fromIntegerMaybe i | otherwise = Just $ fromInteger i
 
 instance SafeNum Int where
   fromIntegerMaybe i = case i of
     IS i -> Just (I# i)
     _ -> Nothing
+
+-- Probably all of these could have hand-coded fast instances
+
+instance SafeNum Int8 where
+  fromIntegerMaybe = fromIntegerBounded
+
+instance SafeNum Int16 where
+  fromIntegerMaybe = fromIntegerBounded
+
+instance SafeNum Int32 where
+  fromIntegerMaybe = fromIntegerBounded
+
+instance SafeNum Int64 where
+  fromIntegerMaybe = fromIntegerBounded
+
+instance SafeNum Word where
+  fromIntegerMaybe = fromIntegerBounded
+
+instance SafeNum Word8 where
+  fromIntegerMaybe = fromIntegerBounded
+
+instance SafeNum Word16 where
+  fromIntegerMaybe = fromIntegerBounded
+
+instance SafeNum Word32 where
+  fromIntegerMaybe = fromIntegerBounded
+
+instance SafeNum Word64 where
+  fromIntegerMaybe = fromIntegerBounded
+
+instance SafeNum Double where
+  fromIntegerMaybe = Just . fromInteger
+
+instance (Integral a, SafeNum a) => SafeNum (Ratio a) where
+  fromIntegerMaybe i = (:%) <$> fromIntegerMaybe i <*> fromIntegerMaybe 1
 
 -----------------
 -- SafeFractional
@@ -55,5 +102,14 @@ class Fractional a => SafeFractional a where
 realToFracMaybe :: (Real a, SafeFractional b) => a -> Maybe b
 realToFracMaybe = fromRationalMaybe . toRational
 
+-- | Implement 'fromRationalMaybe' by converting the numerator and the denominator to 'a's via 'fromIntegerMaybe'
+-- and then using '\/'. Returns 'Nothing' for a 0 denominator. Not universally safe as it relies on '\/' being total
+-- given non-zero denominators.
+fromRationalViaInteger :: forall a . (Fractional a, SafeNum a) => Rational -> Maybe a
+fromRationalViaInteger (n :% 0) = Nothing
+fromRationalViaInteger (n :% d) = (/) <$> fromIntegerMaybe n <*> fromIntegerMaybe d
+
+-- This instance is only sensible if 'Ratio a' is generally well-behaved,
+-- which it often isn't...
 instance (Integral a, SafeNum a) => SafeFractional (Ratio a) where
-  fromRationalMaybe (n :% d) = (:%) <$> fromIntegerMaybe n <*> fromIntegerMaybe d
+  fromRationalMaybe = fromRationalViaInteger
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Main (main) where
 
 import SafeNum
 
 import Test.Tasty
 import Test.QuickCheck
+import Test.QuickCheck.Instances.Natural ()
 import Data.Proxy
 import Control.Exception
 import Data.Either (isRight)
@@ -15,6 +18,13 @@ import Numeric.Natural
 import Data.Typeable
 import Data.Word
 import Data.Int
+import Data.Ratio
+import GHC.Real
+import Data.Complex
+
+-----------
+-- Integral
+-----------
 
 prop_toIntegerTotal :: forall a . (Show a, Arbitrary a, Integral a) => Proxy a -> Property
 prop_toIntegerTotal _ = forAll arbitrary $ \(i :: a) -> ioProperty $ do
@@ -22,13 +32,17 @@ prop_toIntegerTotal _ = forAll arbitrary $ \(i :: a) -> ioProperty $ do
   pure $ isRight i'
 
 prop_fromIntegerToIntegerLeftInverse :: forall a . (Show a, Arbitrary a, Integral a) => Proxy a -> Property
-prop_fromIntegerToIntegerLeftInverse _ = forAll arbitrary $ \(i :: a) -> fromInteger (toInteger i) == i
+prop_fromIntegerToIntegerLeftInverse _ = forAll arbitrary $ \(i :: a) -> fromInteger (toInteger i) === i
 
-toIntegerLaws :: forall a . (Typeable a, Show a, Arbitrary a, Integral a) => Proxy a -> TestTree
-toIntegerLaws p = testGroup ("toInteger laws (" ++ show (typeRep p) ++ ")")
+integralLaws :: forall a . (Typeable a, Show a, Arbitrary a, Integral a) => Proxy a -> TestTree
+integralLaws p = testGroup ("Integral laws for " ++ show (typeRep p))
   [ testProperty "toInteger total" $ prop_toIntegerTotal p
   , testProperty "fromInteger/toInteger left inverse" $ prop_fromIntegerToIntegerLeftInverse p
   ]
+
+----------
+-- SafeNum
+----------
 
 prop_fromIntegerMaybeTotal :: forall a . (Show a, Arbitrary a, SafeNum a) => Proxy a -> Property
 prop_fromIntegerMaybeTotal _ = forAll arbitrary $ \(i :: Integer) -> ioProperty $ do
@@ -37,65 +51,175 @@ prop_fromIntegerMaybeTotal _ = forAll arbitrary $ \(i :: Integer) -> ioProperty 
 
 prop_fromIntegerMaybeFromIntegerAgreement :: forall a . (Show a, Eq a, Arbitrary a, SafeNum a) => Proxy a -> Property
 prop_fromIntegerMaybeFromIntegerAgreement _ = forAll arbitrary $ \(i :: Integer) ->
-  let r = fromIntegerMaybe @a i
-  in cover 30 (isJust r) "in range" $ case r of
-    Just i' -> i' == fromInteger i
+  case fromIntegerMaybe @a i of
+    Just i' -> i' === fromInteger i
     Nothing -> discard
 
 prop_fromIntegerMaybeToIntegerLeftInverse :: forall a . (Show a, Arbitrary a, SafeNum a, Integral a) => Proxy a -> Property
-prop_fromIntegerMaybeToIntegerLeftInverse _ = forAll arbitrary $ \(i :: a) -> fromIntegerMaybe (toInteger i) == Just i
+prop_fromIntegerMaybeToIntegerLeftInverse _ = forAll arbitrary $ \(i :: a) -> fromIntegerMaybe (toInteger i) === Just i
 
 prop_fromIntegerMaybeToIntegerRightInverse :: forall a . (Show a, Arbitrary a, SafeNum a, Integral a) => Proxy a -> Property
 prop_fromIntegerMaybeToIntegerRightInverse _ = forAll arbitrary $ \(i :: Integer) ->
-  let r = fromIntegerMaybe @a i
-  in cover 30 (isJust r) "in range" $ case r of
-    Just i' -> toInteger i' == i
+  case fromIntegerMaybe @a i of
+    Just i' -> toInteger i' === i
     Nothing -> discard
 
-fromIntegerMaybeLaws :: forall a . (Typeable a, Show a, Arbitrary a, SafeNum a, Integral a) => Proxy a -> TestTree
-fromIntegerMaybeLaws p = testGroup ("toInteger laws (" ++ show (typeRep p) ++ ")")
+safeNumLaws :: forall a . (Typeable a, Show a, Arbitrary a, SafeNum a, Integral a) => Proxy a -> TestTree
+safeNumLaws p = testGroup ("SafeNum laws for " ++ show (typeRep p))
   [ testProperty "fromIntegerMaybe total" $ prop_fromIntegerMaybeFromIntegerAgreement p
   , testProperty "fromIntegerMaybe/fromInteger agreement" $ prop_fromIntegerMaybeToIntegerLeftInverse p
-  , testProperty "fromIntegerMaybe/ToInteger left inverse " $ prop_fromIntegerMaybeToIntegerLeftInverse p
+  , testProperty "fromIntegerMaybe/toInteger left inverse " $ prop_fromIntegerMaybeToIntegerLeftInverse p
   , testProperty "fromIntegerMaybe/toInteger right inverse" $ prop_fromIntegerMaybeToIntegerRightInverse p
   ]
 
-toIntegerTests :: TestTree
-toIntegerTests = testGroup "toInteger tests"
-  [ toIntegerLaws (Proxy @Integer)
-  , testGroup "Ints"
-    [ toIntegerLaws (Proxy @Int)
-    , toIntegerLaws (Proxy @Int8)
-    , toIntegerLaws (Proxy @Int16) -- etc
-    ]
-  -- No arbitrary instance?
-  --, toIntegerLaws (Proxy @Natural)
-  , testGroup "Words"
-    [ toIntegerLaws (Proxy @Word)
-    , toIntegerLaws (Proxy @Word8)
-    , toIntegerLaws (Proxy @Word16) -- etc
-    ]
+------------------
+-- Real+Fractional
+------------------
+
+newtype Ratio' a = Ratio' (Ratio a)
+  deriving newtype (Eq, Ord, Num, Real, Fractional, SafeFractional)
+
+arbitraryRatio :: (Show a, Integral a) => Gen a -> Gen (Ratio a)
+arbitraryRatio gen = do
+    n <- gen
+    d <- gen `suchThat` (\n -> n /= 0)
+    pure $ n % d
+
+instance (Show a, Integral a, Arbitrary a) => Arbitrary (Ratio' a) where
+  arbitrary = Ratio' <$> (arbitraryRatio arbitrary)
+
+instance Show a => Show (Ratio' a) where
+  show (Ratio' (n :% d)) = show n ++ " :% " ++ show d
+
+prop_toRationalTotal :: forall a . (Show a, Arbitrary a, Real a) => Proxy a -> Property
+prop_toRationalTotal _ = forAll arbitrary $ \(i :: a) -> ioProperty $ do
+  i' <- try @SomeException $ evaluate $ toRational i
+  pure $ isRight i'
+
+prop_fromRationalToRationalLeftInverse :: forall a . (Show a, Eq a, Arbitrary a, Real a, Fractional a) => Proxy a -> Property
+prop_fromRationalToRationalLeftInverse _ = forAll arbitrary $ \(i :: a) ->
+  fromRational (toRational i) === i
+
+realLaws :: forall a . (Typeable a, Show a, Arbitrary a, Real a) => Proxy a -> TestTree
+realLaws p = testGroup ("Real laws for " ++ show (typeRep p))
+  [ testProperty "toRational total" $ prop_toRationalTotal p
   ]
 
-fromIntegerMaybeTests :: TestTree
-fromIntegerMaybeTests = testGroup "fromIntegerMaybe tests"
-  [ fromIntegerMaybeLaws (Proxy @Integer)
-  , testGroup "Ints"
-    [ fromIntegerMaybeLaws (Proxy @Int)
-    --, fromIntegerMaybeLaws (Proxy @Int8)
-    --, fromIntegerMaybeLaws (Proxy @Int16) -- etc
-    ]
-  -- No arbitrary instance?
-  --, toIntegerLaws (Proxy @Natural)
-  , testGroup "Words"
-    [ --fromIntegerMaybeLaws (Proxy @Word)
-    --, fromIntegerMaybeLaws (Proxy @Word8)
-    --, fromIntegerMaybeLaws (Proxy @Word16) -- etc
-    ]
+realFractionalLaws :: forall a . (Typeable a, Show a, Arbitrary a, Real a, Fractional a) => Proxy a -> TestTree
+realFractionalLaws p = testGroup ("Real+Frational laws (" ++ show (typeRep p) ++ ")")
+  [ testProperty "fromRational/toRational left inverse" $ prop_fromRationalToRationalLeftInverse p
+  ]
+
+-----------------
+-- SafeFractional
+-----------------
+
+prop_fromRationalMaybeTotal :: forall a . (Show a, Arbitrary a, SafeFractional a) => Proxy a -> Property
+prop_fromRationalMaybeTotal _ = forAll arbitrary $ \(i :: Rational) -> ioProperty $ do
+  i' <- try @SomeException $ evaluate $ fromRationalMaybe @a i
+  pure $ isRight i'
+
+prop_fromRationalMaybeFromRationalAgreement :: forall a . (Show a, Eq a, Arbitrary a, SafeFractional a) => Proxy a -> Property
+prop_fromRationalMaybeFromRationalAgreement _ = forAll arbitrary $ \(i :: Rational) ->
+  case fromRationalMaybe @a i of
+    Just i' -> i' === fromRational i
+    Nothing -> discard
+
+prop_fromRationalMaybeToRationalLeftInverse :: forall a . (Show a, Arbitrary a, SafeFractional a, Real a) => Proxy a -> Property
+prop_fromRationalMaybeToRationalLeftInverse _ = forAll arbitrary $ \(i :: a) -> fromRationalMaybe (toRational i) === Just i
+
+prop_fromRationalMaybeToRationalRightInverse :: forall a . (Show a, Arbitrary a, SafeFractional a, Real a) => Proxy a -> Property
+prop_fromRationalMaybeToRationalRightInverse _ = forAll arbitrary $ \(i :: Rational) ->
+  case fromRationalMaybe @a i of
+    Just i' -> toRational i' === i
+    Nothing -> discard
+
+safeFractionalLaws :: forall a . (Typeable a, Show a, Arbitrary a, SafeFractional a, Real a) => Proxy a -> TestTree
+safeFractionalLaws p = testGroup ("SafeFractional laws for " ++ show (typeRep p))
+  [ testProperty "fromRationalMaybe total" $ prop_fromRationalMaybeTotal p
+  , testProperty "fromRationalMaybe/fromRational agreement" $ prop_fromRationalMaybeToRationalLeftInverse p
+  , testProperty "fromRationalMaybe/toRational left inverse " $ prop_fromRationalMaybeToRationalLeftInverse p
+  , testProperty "fromRationalMaybe/toRational right inverse" $ prop_fromRationalMaybeToRationalRightInverse p
+  ]
+
+--------
+-- Tests
+--------
+
+integralTests :: TestTree
+integralTests = testGroup "Integral tests"
+  [ integralLaws (Proxy @Integer)
+  , integralLaws (Proxy @Natural)
+  , integralLaws (Proxy @Int)
+  , integralLaws (Proxy @Int8)
+  , integralLaws (Proxy @Int32)
+  , integralLaws (Proxy @Int64)
+  , integralLaws (Proxy @Word)
+  , integralLaws (Proxy @Word8)
+  , integralLaws (Proxy @Word16)
+  , integralLaws (Proxy @Word32)
+  , integralLaws (Proxy @Word64)
+  ]
+
+safeNumTests :: TestTree
+safeNumTests = testGroup "SafeNum tests"
+  [ safeNumLaws (Proxy @Integer)
+  , safeNumLaws (Proxy @Natural)
+  , safeNumLaws (Proxy @Int)
+  , safeNumLaws (Proxy @Int8)
+  , safeNumLaws (Proxy @Int16)
+  , safeNumLaws (Proxy @Int32)
+  , safeNumLaws (Proxy @Int64)
+  , safeNumLaws (Proxy @Word)
+  , safeNumLaws (Proxy @Word8)
+  , safeNumLaws (Proxy @Word16)
+  , safeNumLaws (Proxy @Word32)
+  , safeNumLaws (Proxy @Word64)
+  ]
+
+realTests :: TestTree
+realTests = testGroup "Real tests"
+  [ realLaws (Proxy @(Ratio' Integer))
+  , realLaws (Proxy @(Ratio' Int))
+  , realLaws (Proxy @Double)
+  , realLaws (Proxy @Float)
+  , realLaws (Proxy @Integer)
+  , realLaws (Proxy @Natural)
+  , realLaws (Proxy @Int)
+  , realLaws (Proxy @Int8)
+  , realLaws (Proxy @Int16)
+  , realLaws (Proxy @Int32)
+  , realLaws (Proxy @Int64)
+  , realLaws (Proxy @Word)
+  , realLaws (Proxy @Word8)
+  , realLaws (Proxy @Word16)
+  , realLaws (Proxy @Word32)
+  , realLaws (Proxy @Word64)
+  ]
+
+realFractionalTests :: TestTree
+realFractionalTests = testGroup "Real+Fractional tests"
+  [ realFractionalLaws (Proxy @(Ratio' Integer))
+  , realFractionalLaws (Proxy @(Ratio' Int))
+  , realFractionalLaws (Proxy @Double)
+  , realFractionalLaws (Proxy @Float)
+  ]
+
+safeFractionalTests :: TestTree
+safeFractionalTests = testGroup "SafeFractional tests"
+  [ safeFractionalLaws (Proxy @(Ratio' Integer))
+  , safeFractionalLaws (Proxy @(Ratio' Int))
   ]
 
 main :: IO ()
 main = defaultMain $ localOption (QuickCheckTests 2000) $ testGroup "safe-num"
-  [ toIntegerTests
-  , fromIntegerMaybeTests
+  [ testGroup "Existing classes"
+    [ integralTests
+    , realTests
+    , realFractionalTests
+    ]
+  , testGroup "Safe classes"
+    [ safeNumTests
+    , safeFractionalTests
+    ]
   ]
